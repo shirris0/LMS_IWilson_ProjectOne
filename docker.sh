39536941d5b40c56c97a446fd30702ae51b46724
Iris Wilson <iriswilsonr@gmail.com>
2021-05-02 17:03:57 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,12 +1,13 @@
+echo "este es otro mensaje desde el shell"
 
-docker stop fwk_fe
+#docker stop fwk_fe
 
-docker rm fwk_fe
+#docker rm fwk_fe
 
-docker rmi fwk_fe
+#docker rmi fwk_fe
 
-docker build -t fwk_fe .
+#docker build -t fwk_fe .
 
-docker run --name fwk_fe -e TZ="America/Mexico_City" --restart always -p 80:80 -p 443:443 fwk_fe
+#docker run --name fwk_fe -e TZ="America/Mexico_City" --restart always -p 80:80 -p 443:443 fwk_fe
 
 #wilson
