27edb2218e03a3f3ba9dbc5609b19c5525dbddd1
Iris Wilson <iriswilsonr@gmail.com>
2021-05-02 17:01:00 -0500

    Update docker.sh

---
+++ docker.sh
@@ -0,0 +1,12 @@
+
+docker stop fwk_fe
+
+docker rm fwk_fe
+
+docker rmi fwk_fe
+
+docker build -t fwk_fe .
+
+docker run --name fwk_fe -e TZ="America/Mexico_City" --restart always -p 80:80 -p 443:443 fwk_fe
+
+#wilson
